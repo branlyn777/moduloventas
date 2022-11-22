d29d0df84c0e4830d51301338837b93affc4efc7
branlyn <branlyn777@gmail.com>
2022-11-22 13:03:26 -0400

    grafico

--- app/Http/Livewire/InicioController.php
+++ app/Http/Livewire/InicioController.php
@@ -4,24 +4,140 @@ namespace App\Http\Livewire;
 
 // use Illuminate\View\Component as ViewComponent;
 
+use App\Models\CompraDetalle;
+use App\Models\Movimiento;
+use App\Models\SaleDetail;
 use App\Models\User;
+use Carbon\Carbon;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 
 class InicioController extends Component
 {
+    public $ventasMes,$ventaMesAnterior,$difVenta;
+    public $comprasMes,$compraMesAnterior,$difCompra;
+    public $ingresosMes,$ingresosMesAnterior,$difIngresos;
+    public $egresosMes,$egresosMesAnterior,$difEgresos;
+    //graficos
+    public $ventas=[],$compras;
     public function render()
     {
 
         $variable = "";
-        $ventas= 
+       
         $users = User::select(DB::raw("COUNT(*) as count"))
         ->whereYear('created_at', date('Y'))
         ->orderBy('id','ASC')
         ->pluck('count');
 
-$labels = $users->keys();
-$data = $users->values();
+        $labels = $users->keys();
+        $data = $users->values();
+        // Calculo de ventas y porcencentajes de diferencia entre el mes actual y el mes anterior
+
+        $this->ventasMes= SaleDetail::whereMonth('created_at', Carbon::now()->format('m'))->get();
+
+        $this->ventasMes= $this->ventasMes->sum(function($value){
+            return $value['quantity']*$value['price'];
+        });
+
+        $this->ventaMesAnterior= SaleDetail::whereMonth('created_at', Carbon::now()->subMonth()->format('m'))->get();
+
+        $this->ventaMesAnterior= $this->ventaMesAnterior->sum(function($value){
+            return $value['quantity']*$value['price'];
+        });
+        if ($this->ventaMesAnterior != 0) {
+          
+            $this->difVenta = ($this->ventasMes/$this->ventaMesAnterior)-1;
+        }
+        else{
+            $this->difVenta=0;
+        }
+
+        //ventas grafico
+
+
+       for ($i=1; $i < 12; $i++) { 
+            $mm= SaleDetail::whereMonth('created_at',$i)->get();
+            $mm=$mm->sum(function($value){
+                return $value['quantity']*$value['price'];
+            });
+       }
+
+
+       // Calculo de compras y porcencentajes de diferencia entre el mes actual y el mes anterior
+        
+        $this->comprasMes= CompraDetalle::whereMonth('created_at', Carbon::now()->format('m'))->get();
+        $this->comprasMes=$this->comprasMes->sum(function($value){
+            return $value['cantidad']*$value['precio'];
+        });
+
+        $this->compraMesAnterior= CompraDetalle::whereMonth('created_at', Carbon::now()->subMonth()->format('m'))->get();
+
+        $this->compraMesAnterior= $this->compraMesAnterior->sum(function($value){
+            return $value['cantidad']*$value['precio'];
+        });
+        if ($this->compraMesAnterior != 0) {
+          
+            $this->difCompra = ($this->comprasMes/$this->compraMesAnterior)-1;
+        }
+        else{
+            $this->difCompra=0;
+        }
+
+
+        // Calculo de ingresos y porcencentajes de diferencia entre el mes actual y el mes anterior
+
+        $this->ingresosMes= Movimiento::join('cartera_movs','cartera_movs.movimiento_id','movimientos.id')
+        ->whereMonth('movimientos.created_at', Carbon::now()->format('m'))->where('cartera_movs.type','INGRESO')
+        ->where('tipoDeMovimiento','EGRESO/INGRESO')
+        ->sum('movimientos.import');    
+       
+
+        $this->ingresosMesAnterior= Movimiento::join('cartera_movs','cartera_movs.movimiento_id','movimientos.id')
+        ->whereMonth('movimientos.created_at', Carbon::now()->subMonth()->format('m'))
+        ->where('cartera_movs.type','INGRESO')
+        ->where('tipoDeMovimiento','EGRESO/INGRESO')
+        ->sum('movimientos.import');
+
+     
+        if ($this->ingresosMesAnterior != 0) {
+          
+            $this->difIngresos = ($this->ingresosMes/$this->ingresosMesAnterior)-1;
+        }
+        else{
+            $this->difIngresos=0;
+        }
+
+
+
+        //calculo de egresos y porcentajes de diferencia entre el mes actual y el mes anterior
+
+        $this->egresosMes= Movimiento::join('cartera_movs','cartera_movs.movimiento_id','movimientos.id')
+        ->whereMonth('movimientos.created_at', Carbon::now()->format('m'))->where('cartera_movs.type','EGRESO')
+        ->where('tipoDeMovimiento','EGRESO/INGRESO')
+        ->sum('movimientos.import');
+       
+
+        $this->egresosMesAnterior= Movimiento::join('cartera_movs','cartera_movs.movimiento_id','movimientos.id')
+        ->whereMonth('movimientos.created_at', Carbon::now()->subMonth()->format('m'))
+        ->where('cartera_movs.type','EGRESO')
+        ->where('tipoDeMovimiento','EGRESO/INGRESO')
+        ->sum('movimientos.import');
+
+     
+        if ($this->egresosMesAnterior != 0) {
+          
+            $this->difEgresos = ($this->egresosMes/$this->egresosMesAnterior)-1;
+        }
+        else{
+            $this->difEgresos=0;
+        }
+
+
+    
+
+
+
 
         return view('livewire.inicio.inicio', [
             'variable' => $variable,
--- resources/views/livewire/inicio/inicio.blade.php
+++ resources/views/livewire/inicio/inicio.blade.php
@@ -2,76 +2,79 @@
 
     <style>
         .zoom {
-        padding: 10px;
-        /* background-color: #ff0083; */
         transition: transform .3s; /* Animation */
-        width: 200px;
-        height: 100px;
-        margin: 0 auto;
-        border-radius: 10px;
-        border: #ffffff solid 2px;
-        color: white;
-        /* text-align: center; */
         cursor: pointer;
-
-   
-       
         }
-
         .zoom:hover {
-        transform: scale(1.3); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+        transform: scale(1.1); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
         }
 </style>
     
 @endsection
-<div>
+<div class="row sales layout-top-spacing">
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+
+   
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-dark bg-primary-gradient zoom">
+                    <div class="card-body pb-0">
+                        <div class="h1 fw-bold float-right">{{round($difVenta)}}%</div>
+                        <h2 class="mb-2">Bs. {{$ventasMes}}</h2>
+                        <p>Total Ventas Mes</p>
+                        <div class="pull-in sparkline-fix chart-as-background">
+                            <div id="lineChart"><canvas width="511" height="70" style="display: inline-block; width: 511.656px; height: 70px; vertical-align: top;"></canvas></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-dark bg-secondary-gradient zoom">
+                    <div class="card-body pb-0">
+                        <div class="h1 fw-bold float-right">{{round($difCompra)}}%</div>
+                        <h2 class="mb-2">Bs. {{$comprasMes}}</h2>
+                        <p>Total Compras Mes</p>
+                        <div class="pull-in sparkline-fix chart-as-background">
+                            <div id="lineChart2"><canvas width="511" height="70" style="display: inline-block; width: 511.656px; height: 70px; vertical-align: top;"></canvas></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-dark bg-success2 zoom">
+                    <div class="card-body pb-0">
+                        <div class="h1 fw-bold float-right">{{round($difIngresos)}}%</div>
+                        <h2 class="mb-2">Bs. {{$ingresosMes}}</h2>
+                        <p>Ingresos</p>
+                        <div class="pull-in sparkline-fix chart-as-background">
+                            <div id="lineChart3"><canvas width="511" height="70" style="display: inline-block; width: 511.656px; height: 70px; vertical-align: top;"></canvas></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-dark bg-info zoom">
+                    <div class="card-body pb-0">
+                        <div class="h1 fw-bold float-right">{{round($difIngresos)}}%</div>
+                        <h2 class="mb-2">Bs. {{$egresosMes}}</h2>
+                        <p>Egresos</p>
+                        <div class="pull-in sparkline-fix chart-as-background">
+                            <div id="lineChart3"><canvas width="511" height="70" style="display: inline-block; width: 511.656px; height: 70px; vertical-align: top;"></canvas></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
 
     <br>
 
 
 
-    <div class="row">
-
-        <div class="col-12 col-sm-6 col-md-3">
-            {{-- <div class="zoom" style="background-color: #ff0083;"> --}}
-            <div class="zoom" style="background-color: #00b749c5;">
-                <h3><b>Total Ventas Mes: </b></h3>
-          
-                <h3>Bs. 0</h3>
-            </div>
-        </div>
-
-        <div class="col-12 col-sm-6 col-md-3">
-            {{-- <div class="zoom" style="background-color: #972300;"> --}}
-            <div class="zoom" style="background-color: #7095f2;">
-                <h3><b>Total Compras Mes:</b></h3>
-                <h3>Bs. 0</h3>
-            </div>
-        </div>
-        <div class="col-12 col-sm-6 col-md-3">
-            {{-- <div class="zoom" style="background-color: #009721;"> --}}
-            <div class="zoom" style="background-color: #4923d1c2;">
-                <h3><b>Ingresos:</b></h3>
-                <h3>Bs. 0</h3>
-            </div>
-        </div>
-
-        <div class="col-12 col-sm-6 col-md-3">
-            {{-- <div class="zoom" style="background-color: #972300;"> --}}
-            <div class="zoom" style="background-color: #a791a992;">
-                <h3><b>Egresos:</b></h3>
-                <h3>Bs. 0</h3>
-            </div>
-        </div>
 
 
-
-    </div>
-
-    <br>
-    <br>
-
-    <div class="row">
+   
 
         {{-- <div class="col-12 col-sm-6 col-md-3 text-center">
           
@@ -89,26 +92,46 @@
 
         <div class="col-12 col-sm-6 col-md-3 text-center">
       
-            <div class="zoom" style="background-color: #02b1ce;">
+            <div class="zoom" style="background-color: #02ce02;">
                 <h2><b>ASIGNAR PERMISOS</b></h2>
             </div>
         </div>
 
         <div class="col-12 col-sm-6 col-md-3 text-center">
   
-            <div class="zoom" style="background-color: #02b1ce;">
+            <div class="zoom" style="background-color: #172022;">
                 <h2><b>CARTERAS</b></h2>
             </div>
         </div> --}}
 
-        <div class="row justify-content-center">
-            <div class="col-lg-12"></div>
-            <div class="row">
-                <h2>Ventas</h2>
-            </div>
-            <div class="row">
+        <div class="row justify-content-center ml-4">
+            <div class="col-lg-12">
 
-            <canvas id="myChart" height="300px"></canvas>
+                <div class="row">
+                   
+                    <div class="col-lg-6">
+                        <div class="row">
+
+                            <div class="col-lg-12">
+        
+                                <h2>Compras y Ventas</h2>
+                            </div>
+                            <canvas id="myChart" height="auto" width='auto'></canvas>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="row">
+
+                            <div class="col-lg-12">
+        
+                                <h2>Ingresos y Egresos</h2>
+                            </div>
+                            <canvas id="myChart2" height="auto" width='auto'></canvas>
+                        </div>
+                    </div>
+
+    
+                </div>
             </div>
         
 
@@ -123,24 +146,51 @@
           
         </div> --}}
 
-    </div>
-
+</div>
+</div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script type="text/javascript">
   
-    var labels =  {{ Js::from($labels) }};
-    var users =  {{ Js::from($data) }};
+    // var labels =  {{ Js::from($labels) }};
+    var ventas =  {{ Js::from($ventas) }};
+    var compras =  {{ Js::from($compras) }};
+    var ingresos =  {{ Js::from($ingresos) }};
+    var egresos =  {{ Js::from($egresos) }};
+    const labels = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Sept.',
+    'Oct.',
+    'Nov.',
+    'Dic.',
   
+];
     const data = {
         labels: labels,
         datasets: [{
             label: 'Venta Bs.',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 60, 100)',
-            data: users,
-        }]
+            backgroundColor: '#02b1ce',
+            borderColor: '#02b1ce',
+            data: ventas,
+            
+        },
+        {
+            label: 'Compra Bs.',
+            backgroundColor: '#172022',
+            borderColor: '#172022',
+         
+            data: compras,
+    }]
+     
     };
   
     const config = {
@@ -148,10 +198,64 @@
         data: data,
         options: {}
     };
+
+
   
     const myChart = new Chart(
         document.getElementById('myChart'),
         config
+    );
+
+
+
+//Grafico de Ingresos y egresos
+
+
+
+    const labels2 = [
+        'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Sept.',
+    'Oct.',
+    'Nov.',
+    'Dic.',
+];
+    const data2 = {
+        labels: labels2,
+        datasets: [{
+            label: 'Ingresos Bs.',
+            backgroundColor: '#02ce02',
+            borderColor: '#02ce02',
+            data: [0, 20, 8, 2],
+            
+        },
+        {
+            label: 'Egresos Bs.',
+            backgroundColor: '#172022',
+            borderColor: '#172022',
+         
+            data: [0, 4, 5, 9],
+    }]
+     
+    };
+  
+    const configs = {
+        type: 'line',
+        data: data2,
+        options: {}
+    };
+
+
+  
+    const myChart2 = new Chart(
+        document.getElementById('myChart2'),
+        configs
     );
   
 </script>
