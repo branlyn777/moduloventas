26d655a8d19658381258c1ca5315e6362476fa95
Branlyn <branlyn777@gmail.com>
2022-12-08 18:35:58 -0400

    asddsa

--- resources/views/livewire/pos/component.blade.php
+++ resources/views/livewire/pos/component.blade.php
@@ -671,7 +671,7 @@
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: 3000,
+            timer: 4000,
             padding: '2em'
             });
             toast({
