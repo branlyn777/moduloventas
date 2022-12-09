2f8dfd485545db22dc2edb84fc9c201dc330c629
Branlyn <branlyn777@gmail.com>
2022-12-09 13:05:27 -0400

    actualisacion de vistas 1

--- resources/views/livewire/unidad/component.blade.php
+++ resources/views/livewire/unidad/component.blade.php
@@ -1,60 +1,69 @@
-<div class="row sales layout-top-spacing">
-    <div class="col-sm-12">
-        <div class="widget widget-chart-one">
-            <div class="widget-heading">
-                <h3 class="text-center">
-                    <b>UNIDADES</b>
-                </h3>
-                <ul class="row justify-content-end">
-                    
-                        <a href="javascript:void(0)" class="boton-azul" data-toggle="modal"
-                        data-target="#theModal">Agregar Unidad</a>
-                    
-                </ul>
-            </div>
-            <div class="row">
-                <div class="col-lg-4">
-                    @include('common.searchbox')
+<div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header pb-0">
+                    <div class="d-lg-flex">
+                        <div>
+                            <h5 class="mb-0">Unidades</h5>
+                        </div>
 
+                        <div class="ms-auto my-auto mt-lg-0 mt-4">
+                            <div class="ms-auto my-auto">
+                                <a href="javascript:void(0)" class="btn bg-gradient-primary btn-sm mb-0" data-toggle="modal"
+                                data-target="#theModal">Agregar Unidad</a>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="d-lg-flex">
+                        <div class="col-12 col-sm-12 col-md-3">
+                            @include('common.searchbox')
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <div class="widget-content">
-                <div class="table-6">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th class="text-center">ITEM</th>
-                                <th class="text-center">NOMBRE</th>                              
-                                <th class="text-center">ACCIONES</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($data_unidad as $data)
-                                <tr>
-                                    <td>
-                                        <h6 class="text-center">{{ $data->id }}</h6>
-                                    </td>
-                                    <td>
-                                        <h6 class="text-center">{{ $data->nombre }}</h6>
-                                    </td>
-                                    
-                                    <td class="text-center">
-                                        <a href="javascript:void(0)" wire:click="Edit({{ $data->id }})"
-                                            class="boton-azul" title="Editar Unidad">
-                                            <i class="fas fa-edit"></i>
-                                        </a>
-                                        <a href="javascript:void(0)" onclick="Confirm('{{ $data->id }}','{{ $data->nombre }}')" 
-                                            class="boton-rojo" title="Eliminar unidad">
-                                            <i class="fas fa-trash"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $data_unidad->links() }}
+            
+                <div class="card-body px-0 pb-0">
+                    <div class="table-responsive">
+                        <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+                            <div class="dataTable-container">
+                                <table class="table align-items-center mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">ITEM</th>
+                                            <th class="text-center">NOMBRE</th>                              
+                                            <th class="text-center">ACCIONES</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($data_unidad as $data)
+                                            <tr>
+                                                <td>
+                                                    <h6 class="text-center">{{ $data->id }}</h6>
+                                                </td>
+                                                <td>
+                                                    <h6 class="text-center">{{ $data->nombre }}</h6>
+                                                </td>
+                                                
+                                                <td class="text-center">
+                                                    <a href="javascript:void(0)" wire:click="Edit({{ $data->id }})"
+                                                        class="boton-azul" title="Editar Unidad">
+                                                        <i class="fas fa-edit"></i>
+                                                    </a>
+                                                    <a href="javascript:void(0)" onclick="Confirm('{{ $data->id }}','{{ $data->nombre }}')" 
+                                                        class="boton-rojo" title="Eliminar unidad">
+                                                        <i class="fas fa-trash"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                {{ $data_unidad->links() }}
             </div>
         </div>
     </div>
