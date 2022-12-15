4b03c9ea73d4a3672cfa257756d1723ada9332ec
Branlyn <branlyn777@gmail.com>
2022-12-15 16:21:17 -0400

    Cambios Modal Categoria Movimiento Cartera

--- resources/views/common/modalHead.blade.php
+++ resources/views/common/modalHead.blade.php
@@ -3,7 +3,9 @@
     <div class="modal-content">
       <div class="modal-header bg-primary">
         <h1 class="modal-title fs-5 text-white" id="exampleModalLabel">
-          {{$componentName}}</b> | {{$selected_id > 0 ? 'EDITAR':'CREAR'}}
+          <p class="text-sm mb-0">
+            {{$componentName}}</b> | {{$selected_id > 0 ? 'EDITAR':'CREAR'}}
+          </p>
         </h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
   </div>
--- resources/views/livewire/carteramovcategoria/modalnuevacategoria.blade.php
+++ resources/views/livewire/carteramovcategoria/modalnuevacategoria.blade.php
@@ -1,51 +1,66 @@
-@include('common.modalHead')
-<div class="row">
-    <div class="col-sm-12">
-      <h5 class="modal-title" id="exampleModalLongTitle">
+
+<!-- Modal -->
+<div wire:ignore.self class="modal fade" id="modalnuevacategoria" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header bg-primary">
+        <h1 class="modal-title fs-5 text-white" id="exampleModalLabel">
+          <p class="text-sm mb-0">
+            @if($this->categoria_id == null)
+            Crear Categoria
+            @else
+            Actualizar Categoria
+            @endif
+          </p>
+        </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+      <div class="modal-body">
+          <form>
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Nombre Categoria</label>
+                <input wire:model.lazy="nombrecategoria" type="email" class="form-control" id="exampleFormControlInput1" placeholder="Ingrese nombre de la categoria">
+                @error('nombrecategoria')
+                  <span class="text-danger er">{{ $message }}</span>
+                @enderror
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">Detalle Categoria</label>
+                <textarea wire:model.lazy="detallecategoria" placeholder="Ingrese las caracteristicas de la categoria" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Tipo</label>
+                <select wire:model="tipo" class="form-select" aria-label="Default select example">
+                  <option value="Elegir">Seleccione a donde irá la categoria</option>
+                  <option value="INGRESO">INGRESO</option>
+                  <option value="EGRESO">EGRESO</option>
+                </select>
+              </div>
+              @error('tipo')
+                <span class="text-danger er">{{ $message }}</span>
+              @enderror
+            </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+          <p class="text-sm mb-0">
+            Cerrar
+          </p>
+        </button>
         @if($this->categoria_id == null)
-        Crear Categoria
+        <button wire:click="save()" type="button" class="btn btn-primary">
+          <p class="text-sm mb-0">
+            Guardar Cambios
+          </p>
+        </button>
         @else
-        Actualizar Categoria
+        <button wire:click="update()" type="button" class="btn btn-primary">
+          <p class="text-sm mb-0">
+            Actualizar Cambios
+          </p>
+        </button>
         @endif
-      </h5>
-        <div class="form-group">
-          <h6>Nombre Categoria</h6>
-            <div class="input-group mb-4">
-                <span class="input-group-text"><i class="fas fa-edit"></i></span>
-                <input type="text" wire:model="nombrecategoria" placeholder="Ingrese nombre de la categoria" class="form-control ">
-            </div>
-        </div>
-        @error('nombrecategoria')<span class="text-danger er">{{ $message }}</span> @enderror
-
-
-        <div class="row">
-          <h6>Detalle Categoria</h6>
-          <div class="col-12">
-              <textarea class="col-12" wire:model.lazy="detallecategoria" placeholder="Ingrese las caracteristicas de la categoria" maxlength="500" rows="10"></textarea>
-          </div>
-        </div>
-
-
-        <div class="form-group">
-          <label for="exampleFormControlInput1">Tipo</label>
-          <select wire:model="tipo" class="form-control" aria-label="Default select example">
-            <option value="Elegir">Seleccione a donde irá la categoria</option>
-            <option value="INGRESO">INGRESO</option>
-            <option value="EGRESO">EGRESO</option>
-          </select>
-        </div>
-        @error('tipo')<span class="text-danger er">{{ $message }}</span>@enderror
+      </div>
     </div>
-</div>
-</div>
-
-<div class="modal-footer">
-  <button type="button" class="btn btn-secondary" data-dismiss="modal">CANCELAR</button>
-@if ($this->categoria_id == null)
-    <button wire:click.prevent="save()" type="button" class="btn btn-primary">  GUARDAR </button>
-@else
-
-    <button type="button" wire:click.prevent="update()" class="btn btn-primary"> ACTUALIZAR</button>
-@endif
-
+  </div>
 </div>
