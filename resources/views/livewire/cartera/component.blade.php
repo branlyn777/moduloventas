169cee3655cb502c1ca58c7581817fa214af6fc4
ricardothq <ricardothq@gmail.com>
2022-12-15 15:06:29 -0400

    Cambios Cartera

--- resources/views/livewire/cartera/component.blade.php
+++ resources/views/livewire/cartera/component.blade.php
@@ -1,8 +1,6 @@
 <div>
     <div class="d-sm-flex justify-content-between">
-        <div class="col-12 col-sm-12 col-md-4">
-            @include('common.searchbox')
-        </div>
+        <div></div>
         <div class="nav-wrapper position-relative end-0">
             <button wire:click="Agregar()" class="btn btn-icon btn-outline-white ms-2 export" data-type="csv"
                 type="button">
@@ -10,7 +8,7 @@
                     <i class="ni ni-fat-add"></i>
                 </span class="btn-inner--text">Nueva Cartera</button>
 
-                <a href="carteramovcategoria" class="btn btn-icon btn-outline-white ms-2 export" data-type="csv"
+            <a href="carteramovcategoria" class="btn btn-icon btn-outline-white ms-2 export" data-type="csv"
                 type="button">
                 <span class="btn-inner--icon">
                     <i class="ni ni-fat-add"></i>
@@ -19,57 +17,65 @@
             </a>
         </div>
     </div>
+    <br>
     <div class="row">
         <div class="col-12">
             <div class="card mb-4">
                 <div class="card-header pb-0">
                     <h6>Carteras | Listado</h6>
                 </div>
-                <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
-                            <thead>
-                                <tr class="text-center">
-                                    <th class="text-center text-uppercase text-xxs font-weight-bolder">NOMBRE</th>
-                                    <th class="text-center text-uppercase text-xxs font-weight-bolder">DESCRIPCION</th>
-                                    <th class="text-center text-uppercase text-xxs font-weight-bolder">TIPO</th>
-                                    {{-- <th>NÚMERO TELEFONO</th> --}}
-                                    <th class="text-center text-uppercase text-xxs font-weight-bolder">CAJA</th>
-                                    <th class="text-center text-uppercase text-xxs font-weight-bolder">ACCIONES</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data as $item)
+                <div style="padding-left: 12px; padding-right: 12px;">
+                    <div class="col-12 col-sm-12 col-md-4">
+                        @include('common.searchbox')
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <thead>
                                     <tr class="text-center">
-                                        <td class="text-xs mb-0 text-center">
-                                            {{ $item->nombre }}
-                                        </td>
-                                        <td class="text-xs mb-0 text-center">
-                                            {{ $item->descripcion }}
-                                        </td>
-                                        <td class="text-xs mb-0 text-center">
-                                            {{ $item->tipo }}
-                                        </td>
-                                        {{-- <td>
+                                        <th class="text-center text-uppercase text-xxs font-weight-bolder">NOMBRE</th>
+                                        <th class="text-center text-uppercase text-xxs font-weight-bolder">DESCRIPCION
+                                        </th>
+                                        <th class="text-center text-uppercase text-xxs font-weight-bolder">TIPO</th>
+                                        {{-- <th>NÚMERO TELEFONO</th> --}}
+                                        <th class="text-center text-uppercase text-xxs font-weight-bolder">CAJA</th>
+                                        <th class="text-center text-uppercase text-xxs font-weight-bolder">ACCIONES</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data as $item)
+                                        <tr class="text-center">
+                                            <td class="text-xs mb-0 text-center">
+                                                {{ $item->nombre }}
+                                            </td>
+                                            <td class="text-xs mb-0 text-center">
+                                                {{ $item->descripcion }}
+                                            </td>
+                                            <td class="text-xs mb-0 text-center">
+                                                {{ $item->tipo }}
+                                            </td>
+                                            {{-- <td>
                                             {{ $item->telefonoNum }}
                                         </td> --}}
-                                        <td class="text-xs mb-0 text-center">
-                                            {{ $item->caja->nombre }}
-                                        </td>
-                                        <td class="align-middle text-center">
-                                            <a href="javascript:void(0)" wire:click="Edit({{ $item->id }})" class="mx-3"
-                                                title="Editar">
-                                                <i class="fas fa-user-edit text-info" aria-hidden="true"></i>
-                                            </a>
-                                            <a href="javascript:void(0)" onclick="Confirm('{{ $item->id }}','{{ $item->nombre }}','{{ $item->movimientos }}')"
-                                                class="mx-3" title="Borrar">
-                                                <i class="fas fa-trash text-danger" aria-hidden="true"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                            <td class="text-xs mb-0 text-center">
+                                                {{ $item->caja->nombre }}
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <a href="javascript:void(0)" wire:click="Edit({{ $item->id }})"
+                                                    class="mx-3" title="Editar">
+                                                    <i class="fas fa-user-edit text-info" aria-hidden="true"></i>
+                                                </a>
+                                                <a href="javascript:void(0)"
+                                                    onclick="Confirm('{{ $item->id }}','{{ $item->nombre }}','{{ $item->movimientos }}')"
+                                                    class="mx-3" title="Borrar">
+                                                    <i class="fas fa-trash text-danger" aria-hidden="true"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
