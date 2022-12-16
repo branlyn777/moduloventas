31664161ff111af506ae2d50b9ca95d4bf8af734
xio_dev <roscee777@gmail.com>
2022-12-16 17:14:52 -0400

    cambiojoscerrarsesion

--- resources/views/layouts/theme/header.blade.php
+++ resources/views/layouts/theme/header.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-main navbar-expand-lg  px-0 mx-4 shadow-none border-radius-xl z-index-sticky " id="navbarBlur"
 	data-scroll="false">
 	<div class="container-fluid py-1 px-3">
-		<nav aria-label="breadcrumb">
+		{{-- <nav aria-label="breadcrumb">
 			<ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
 				<li class="breadcrumb-item text-sm">
 					<a class="text-white" href="javascript:;">
@@ -14,7 +14,7 @@
 				<li class="breadcrumb-item text-sm text-white active" aria-current="page">User</li>
 			</ol>
 			<h6 class="font-weight-bolder mb-0 text-white">Usuarios</h6>
-		</nav>
+		</nav> --}}
 		<div class="sidenav-toggler sidenav-toggler-inner d-xl-block d-none ">
 			<a href="javascript:;" class="nav-link p-0">
 				<div class="sidenav-toggler-inner">
@@ -36,8 +36,8 @@
 				<li class="nav-item d-flex align-items-center">
 					<a href="../../pages/authentication/signin/illustration.html"
 						class="nav-link text-white font-weight-bold px-0" target="_blank">
-						<i class="fa fa-user me-sm-1" aria-hidden="true"></i>
-						<span class="d-sm-inline d-none">Gustavo</span>
+						
+						<span class="d-sm-inline d-none">{{auth()->user()->name}}</span>
 					</a>
 				</li>
 				<li class="nav-item d-xl-none ps-3 d-flex align-items-center">
@@ -57,82 +57,52 @@
 				<li class="nav-item dropdown pe-2 d-flex align-items-center">
 					<a href="javascript:;" class="nav-link text-white p-0" id="dropdownMenuButton"
 						data-bs-toggle="dropdown" aria-expanded="false">
-						<i class="fa fa-bell cursor-pointer" aria-hidden="true"></i>
+						<i class="fa fa-user me-sm-1" aria-hidden="true"></i>
 					</a>
 					<ul class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
 						<li class="mb-2">
 							<a class="dropdown-item border-radius-md" href="javascript:;">
 								<div class="d-flex py-1">
 									<div class="my-auto">
-										<img src="../.img/team-2.jpg" class="avatar avatar-sm  me-3 " alt="user image">
+										<img src="{{ asset('storage/usuarios/' . auth()->user()->image) }}" class="avatar avatar-sm  me-3 " alt="user image">
 									</div>
 									<div class="d-flex flex-column justify-content-center">
 										<h6 class="text-sm font-weight-normal mb-1">
-											<span class="font-weight-bold">New message</span> from Laur
+											<span class="font-weight-bold">{{auth()->user()->name}}</span> <br> {{auth()->user()->profile}}
 										</h6>
-										<p class="text-xs text-secondary mb-0">
-											<i class="fa fa-clock me-1" aria-hidden="true"></i>
-											13 minutes ago
+										<p class="text-xs text-secondary mb-0">									
+											{{auth()->user()->email}}
 										</p>
 									</div>
 								</div>
 							</a>
 						</li>
-						<li class="mb-2">
-							<a class="dropdown-item border-radius-md" href="javascript:;">
-								<div class="d-flex py-1">
-									<div class="my-auto">
-										<img src="../.img/small-logos/logo-spotify.svg"
-											class="avatar avatar-sm bg-gradient-dark  me-3 " alt="logo spotify">
-									</div>
-									<div class="d-flex flex-column justify-content-center">
-										<h6 class="text-sm font-weight-normal mb-1">
-											<span class="font-weight-bold">New album</span> by Travis Scott
-										</h6>
-										<p class="text-xs text-secondary mb-0">
-											<i class="fa fa-clock me-1" aria-hidden="true"></i>
-											1 day
-										</p>
-									</div>
-								</div>
-							</a>
-						</li>
+										
 						<li>
-							<a class="dropdown-item border-radius-md" href="javascript:;">
+							<a class="dropdown-item border-radius-md" href="{{ route('logout') }}" 
+							onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
 								<div class="d-flex py-1">
-									<div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">
-										<svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
-											xmlns="http://www.w3.org/2000/svg"
-											xmlns:xlink="http://www.w3.org/1999/xlink">
-											<title>credit-card</title>
-											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-												<g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF"
-													fill-rule="nonzero">
-													<g transform="translate(1716.000000, 291.000000)">
-														<g transform="translate(453.000000, 454.000000)">
-															<path class="color-background"
-																d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z"
-																opacity="0.593633743"></path>
-															<path class="color-background"
-																d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z">
-															</path>
-														</g>
-													</g>
-												</g>
-											</g>
-										</svg>
-									</div>
+									<div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">									
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+										fill="none" stroke="currentColor" stroke-widht="2" stroke-linecap="round"
+										stroke_linejoin="round" class="feather feather-log-out"> <title>Cerrar Sesión</title> <path d="M9 21H5a2 2 0 0
+										1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline>
+										<line x1="21" y1="12" x2="9" y2="12"></line></svg>
+
+				
+
+									</div>							
 									<div class="d-flex flex-column justify-content-center">
 										<h6 class="text-sm font-weight-normal mb-1">
-											Payment successfully completed
-										</h6>
-										<p class="text-xs text-secondary mb-0">
-											<i class="fa fa-clock me-1" aria-hidden="true"></i>
-											2 days
-										</p>
+											Cerrar Sesión
+										</h6>									
 									</div>
 								</div>
 							</a>
+							<form action="{{ route('logout') }}" method="POST" id="logout-form">
+								@csrf
+							</form>
+
 						</li>
 					</ul>
 				</li>
