9db4bf0a38439a128b0aaa0f1e1a736fd2560282
Branlyn <branlyn777@gmail.com>
2022-12-23 15:48:08 -0400

    Cambios Ingeniero

--- resources/views/layouts/theme/header.blade.php
+++ resources/views/layouts/theme/header.blade.php
@@ -2,6 +2,7 @@
 <nav class="navbar navbar-main navbar-expand-lg  px-0 mx-4 shadow-none border-radius-xl z-index-sticky " id="navbarBlur"
     data-scroll="false">
     <div class="container-fluid py-1 px-3">
+        @yield('migaspan')
         {{-- <nav aria-label="breadcrumb">
 			<ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
 				<li class="breadcrumb-item text-sm">
