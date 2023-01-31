b091adcaa8db56e18961bc14fff3eb872f20aa23
ricardothq <ricardothq@gmail.com>
2023-01-31 12:26:12 -0400

    Cambios de Origen Motivo 2

--- resources/views/livewire/origen_motivo/modalDetails.blade.php
+++ resources/views/livewire/origen_motivo/modalDetails.blade.php
@@ -1,16 +1,18 @@
-<div wire:ignore.self id="modal-details" class="modal fade" tabindex="1" role="dialog">
-    <div class="modal-dialog modal-lg" role="document">
+<div wire:ignore.self class="modal fade" id="modal-details" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header bg-dark">
-                <h5 class="modal-title text-white">
-                    <b>{{ $origenNombre }} | {{ $motivoNombre }}</b>
-                </h5>
-                <button class="close" data-dismiss="modal" type="button" aria-label="Close">
-                    <span class="text-white">&times;</span>
+            <div class="modal-header bg-primary">
+                <div>
+                    <h5 class="mb-0 text-white" style="font-size: 16px">{{ $origenNombre }} | {{ $motivoNombre }}</h5>
+                </div>
+                <button type="button" class="btn-close fs-3" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
-            </div>
 
+            </div>
             <div class="modal-body">
+
                 <div class="row">
                     <div class="col-sm-12 col-md-12">
                         <div class="form-group">
@@ -107,8 +109,9 @@
 
                                             <div class="n-chk">
                                                 <label class="new-control new-radio radio-classic-primary">
-                                                    <input type="radio" class="new-control-input" name="custom-radio-3"
-                                                        id="SI" value="suma" wire:model="suma_resta_no">
+                                                    <input type="radio" class="new-control-input"
+                                                        name="custom-radio-3" id="SI" value="suma"
+                                                        wire:model="suma_resta_no">
                                                     <span class="new-control-indicator"></span>SUMA
                                                 </label>
 
@@ -120,8 +123,9 @@
 
                                             <div class="n-chk">
                                                 <label class="new-control new-radio radio-classic-primary">
-                                                    <input type="radio" class="new-control-input" name="custom-radio-3"
-                                                        id="NO" value="resta" wire:model="suma_resta_no">
+                                                    <input type="radio" class="new-control-input"
+                                                        name="custom-radio-3" id="NO" value="resta"
+                                                        wire:model="suma_resta_no">
                                                     <span class="new-control-indicator"></span>RESTA
                                                 </label>
                                             </div>
@@ -132,8 +136,10 @@
 
                                             <div class="n-chk">
                                                 <label class="new-control new-radio radio-classic-primary">
-                                                    <input type="radio" class="new-control-input" name="custom-radio-3"
-                                                        id="NO" value="mantiene" wire:model="suma_resta_no">
+                                                    <input type="radio" class="new-control-input"
+                                                        name="custom-radio-3" id="NO" value="mantiene"
+                                                        wire:model="suma_resta_no">
+
                                                     <span class="new-control-indicator"></span>MANTIENE
                                                 </label>
                                             </div>
@@ -149,8 +155,8 @@
                     <div class="col-sm-12 col-md-6">
                         <div class="form-group">
                             <label>Afectado</label>
-                            <select wire:model.lazy="afectadoSi" class="form-control">
-                                <option value="Elegir" selected>Elegir</option>
+                            <select wire:model.lazy="afectadoSi" class="form-select">
+                                <option value="Elegir" disabled selected>Elegir</option>
                                 <option value="montoR">Monto a Registrar</option>
                                 <option value="montoC">Monto a Cobrar</option>
                                 <option value="ambos">Ambos</option>
@@ -164,8 +170,8 @@
                         <div class="col-sm-12 col-md-6">
                             <div class="form-group">
                                 <label>Afectado</label>
-                                <select wire:model.lazy="afectadoNo" class="form-control">
-                                    <option value="Elegir" selected>Elegir</option>
+                                <select wire:model.lazy="afectadoNo" class="form-select">
+                                    <option value="Elegir" disabled selected>Elegir</option>
                                     <option value="montoR">Monto a Registrar</option>
                                     <option value="montoC">Monto a Cobrar</option>
                                     <option value="ambos">Ambos</option>
@@ -179,8 +185,8 @@
                     <div class="col-sm-12 col-md-4">
                         <div class="form-group">
                             <label>¿CI DE CLIENTE?</label>
-                            <select wire:model.lazy="CIdeCliente" class="form-control">
-                                <option value="Elegir" selected>Elegir</option>
+                            <select wire:model.lazy="CIdeCliente" class="form-select">
+                                <option value="Elegir" disabled selected>Elegir</option>
                                 <option value="SI">SI</option>
                                 <option value="NO">NO</option>
                             </select>
@@ -193,8 +199,8 @@
                     <div class="col-sm-12 col-md-4">
                         <div class="form-group">
                             <label>¿Teléfono Solicitante?</label>
-                            <select wire:model.lazy="telefSolicitante" class="form-control">
-                                <option value="Elegir" selected>Elegir</option>
+                            <select wire:model.lazy="telefSolicitante" class="form-select">
+                                <option value="Elegir"disabled selected>Elegir</option>
                                 <option value="SI">SI</option>
                                 <option value="NO">NO</option>
                             </select>
@@ -207,8 +213,8 @@
                     <div class="col-sm-12 col-md-4">
                         <div class="form-group">
                             <label>¿Teléfono Destino / Código?</label>
-                            <select wire:model.lazy="telefDestino_codigo" class="form-control">
-                                <option value="Elegir" selected>Elegir</option>
+                            <select wire:model.lazy="telefDestino_codigo" class="form-select">
+                                <option value="Elegir" disabled selected>Elegir</option>
                                 <option value="SI">SI</option>
                                 <option value="NO">NO</option>
                             </select>
@@ -218,8 +224,12 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" wire:click.prevent="Asignar()" class="btn btn-warning close-btn text-info"
-                            style="background: #3b3f5c">Terminar</button>
+                        <button wire:click.prevent="Asignar()" type="button" class="btn btn-primary"
+                            style="font-size: 13px">
+                            Terminar
+                        </button>
+                        {{-- <button type="button" wire:click.prevent="Asignar()" class="btn btn-warning close-btn text-info"
+                            style="background: #3b3f5c">Terminar</button> --}}
                     </div>
                 </div>
             </div>
